88ecbe0562c67dc0c450a928c91b43d0f9a3223b
Larry Adams <professorlarryadams@gmail.com>
2013-09-13 19:12:44 -0400

    Added main index page

---
+++ index.php
@@ -0,0 +1,61 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>CSCI E-15 Homework - P1</title>
+<link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<div class="container">
+
+<!-- end .header -->
+  <div class="header">
+  <h2>All Things European</h2>
+    
+  </div>
+  
+  <!-- Start of Content -->
+  
+  <div class="content">
+   <h1>Hello World from Larry Adams</h1> 
+  
+  	<p>I am a graduate student at Harvard University Extension School.  I am  pursuing a graduate certificate in <a href=				"http://www.extension.harvard.edu/degrees-certificates/professional-certificates/web-technologies-certificate" title="Graduate Certitifcate" target="_blank">Web Technologies</a> and later a <a href="http://www.extension.harvard.edu/degrees-certificates/information-technology/degree-requirements/concentrations#digital" title="Masters degree" target="_blank">Master in Digital Media Arts and Instructional Design</a>.</p>
+  
+  	<p>I live in the Shenandoah Valley which is located near Winchester, Virginia. I enjoy traveling with my family, visiting local wineries, and fall festivals throughout Virginia.  My hobbies include camping, biking, hiking, and working with computers - yes I am a nerd!</p>
+  
+  <p> I work as a IT Developer for the US Coast Guard and teach part time at several universities in the field of computer science (Networking, Security, Database, and Programming).
+  </p> 
+    
+  <h3>My favorite WYSIWYG is DreamWeaver 6</h3>
+  
+  <!-- Validation W3C HTML 5 and CSS3 -->
+  
+  <div>
+  <p>
+<a href="http://jigsaw.w3.org/css-validator/check/referer">
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" />
+</a>
+</p>
+
+
+<p>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            alt="Valid CSS!" />
+    </a>
+</p>
+            </div>
+
+    
+    <!-- end .content --></div>
+  <div class="footer">
+    <h2>Projects: 1 | 2 | 3 | 4</h2>
+    <!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+</html>
