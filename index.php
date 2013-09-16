3251ffaf050974ed968041b3e4455580990629b6
Larry Adams <professorlarryadams@gmail.com>
2013-09-16 19:08:47 -0400

    Adding picture of self

--- index.php
+++ index.php
@@ -14,18 +14,17 @@
 
 <div class="container">
 
-	<div class="header">
-  	<h2>Project 1</h2>
-  	<h3>CSCI E 15</h3>
-    </div>
+<!-- Header -->
+<div class="header"><h2>CSCI E15 Project 1</h2></div>
   
   <!-- Start of Content -->
   
   <div class="content">
    <h1>Hello World from Larry Adams!</h1> 
+   
+	<p><img src="images/la.png" alt="Larry's image" id="Larry" style="background-color: #FFFFFF"></p>
   
-  	<p>I am a graduate student at Harvard University Extension School.  I am  pursuing a graduate certificate in <a href=				"http://www.extension.harvard.edu/degrees-certificates/professional-certificates/web-technologies-certificate" title="Graduate Certitifcate" target="_blank">Web Technologies</a> and later a <a href="http://www.extension.harvard.edu/degrees-certificates/information-technology/degree-requirements/concentrations#digital" title="Masters degree" target="_blank">Master in Digital Media Arts and Instructional Design</a>.</p>
-  
+  	<p>I am a graduate student at Harvard University Extension School.  I am  pursuing a graduate certificate in <a href=				"http://www.extension.harvard.edu/degrees-certificates/professional-certificates/web-technologies-certificate" title="Graduate Certitifcate" target="_blank">Web Technologies</a> and later a <a href="http://www.extension.harvard.edu/degrees-certificates/information-technology/degree-requirements/concentrations#digital" title="Masters degree" target="_blank">Master in Digital Media Arts and Instructional Design</a>. </p>
   	<p>I live in the Shenandoah Valley which is located near Winchester, Virginia. I enjoy traveling with my family, visiting local wineries, and fall festivals throughout Virginia.  My hobbies include camping, biking, hiking, and working with computers - yes I am a nerd!</p>
   
   <p> I work as a IT Developer for the US Coast Guard and teach part time at several universities in the field of computer science (Networking, Security, Database, and Programming).
@@ -36,15 +35,6 @@
   <!-- Validation W3C HTML 5 and CSS3 -->
   
   <div>
-  <p>
-	<a href="http://jigsaw.w3.org/css-validator/check/referer">
-    <img style="border:0;width:88px;height:31px"
-        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
-        alt="Valid CSS!" />
-	</a>
-  </p>
-
-
 <p>
     <a href="http://jigsaw.w3.org/css-validator/check/referer">
         <img style="border:0;width:88px;height:31px"
@@ -52,14 +42,27 @@
             alt="Valid CSS!" />
     </a>
 </p>
-           
- </div>
 
-    <!-- end .content --></div>
+<p>
+<a href="http://jigsaw.w3.org/css-validator/check/referer">
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" />
+</a>
+</p>
+             
+ 
+</div> 
+
+<!-- end .content -->
+
+</div>
   
   <div class="footer">
     <h2>Projects: 1 | 2 | 3 | 4</h2>
     <!-- end .footer --></div>
+    
   <!-- end .container --></div>
+
 </body>
 </html>
